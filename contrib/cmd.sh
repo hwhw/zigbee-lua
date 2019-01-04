e27b3bcefc1a7b86032e5bb32578801127ac2484
Hans-Werner Hilse <hwhilse@gmail.com>
2019-01-04 17:52:36 +0000

    add return value printing to contrib/cmd.sh

--- contrib/cmd.sh
+++ contrib/cmd.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo 'require"interfaces.zigbee.any":new{id="'$1'"}:'$2 | nc -N ${ZIGBEELUA:-127.0.0.1} 16580
+echo 'local ctx,o=...; local U=require"lib.util"; o:write(U.dump(require"interfaces.zigbee.any":new{id="'"$1"'"}:'"$2"'))' | nc -N ${ZIGBEELUA:-127.0.0.1} 16580
