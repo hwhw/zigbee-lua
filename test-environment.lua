7732bc665e05bac9a207e9280217d711cca641bc
Hans-Werner Hilse <hwhilse@gmail.com>
2019-06-07 10:40:06 +0200

    test environment should be empty: clear leftovers

--- test-environment.lua
+++ test-environment.lua
@@ -1,21 +1,8 @@
 #!/usr/bin/env -S luajit
 require"lib.ctx"(require"config", function(ctx)
 local U = require"lib.util"
-
-local E = U.object:new()
-
 local Z = ctx.interfaces.zigbee[1]
-local devices = Z.devices:names()
-for ieeeaddr, d in pairs(devices) do
-  local dev = Z.devices:ieee(ieeeaddr)
-  E[d] = dev.zcl
-end
 
-local lastdev = nil
-local cur_sets = {}
-local function set_lastdev(dev)
-  lastdev = dev
-end
+U.DEBUG("env", "test environment running.")
 
-return E
 end)
